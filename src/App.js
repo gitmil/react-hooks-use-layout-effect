ba3dbd149730423b75e8ab9b29c422932a830efb
techsithgit <hemilp@tuplecorp.com>
2019-09-18 08:43:11 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useLayoutEffect } from "react";
+import "./App.css";
+
+function useDim(el, val) {
+  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    let boundingBox = el.current.getBoundingClientRect();
+    setHeight(boundingBox.height);
+    setWidth(boundingBox.width);
+  }, [val]);
+
+  return { height, width };
+}
 
 function App() {
+  const [val, setVal] = useState(2);
+  const valEl = useRef(null);
+
+  const { height, width } = useDim(valEl, val);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Height: {height} , width: {width}
+        </h1>
+        <div ref={valEl}>{val}</div>
+        <button onClick={() => setVal(val * 10)}>10*</button>
       </header>
     </div>
   );
